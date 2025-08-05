447afc5db3724190887342bafc126ae1aea65677
raghd <raghadayyad2004@gmail.com>
2025-08-05 12:31:13 +0200

    test is done

--- function.dart
+++ function.dart
@@ -1,8 +1,8 @@
 
 void main (){
-
-
+greet(name: "raghd");
+greet(name: "reema" ,greeting: "Welcome");
 }
- void greet({required String name ,String greeting  =  'hallo' }){
+ void greet({required String name ,String greeting  =  'Hello' }){
   print('$greeting , $name '); 
  }
