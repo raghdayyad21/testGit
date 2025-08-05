1d4790c6c2320c960e3bd652dd35cb5b63bc0d8d
raghd <raghadayyad2004@gmail.com>
2025-08-05 12:46:39 +0200

    registerUser is create

--- function.dart
+++ function.dart
@@ -2,7 +2,14 @@
 void main (){
 greet(name: "raghd");
 greet(name: "reema" ,greeting: "Welcome");
+String user =registerUser(username: "raghs", email: "raghd ayyad");
+
 }
+
  void greet({required String name ,String greeting  =  'Hello' }){
   print('$greeting , $name '); 
+ }
+
+ String registerUser({required String username ,required String email}){
+   return "User $username registered with email $email";
  }
