92bf841809e8c4abbcbfd44f590111acf9e642db
raghd <raghadayyad2004@gmail.com>
2025-08-05 13:11:08 +0200

    printItems is done

--- function.dart
+++ function.dart
@@ -3,7 +3,8 @@ void main (){
 greet(name: "raghd");
 greet(name: "reema" ,greeting: "Welcome");
 registerUser(username: "raghd", email: "raghdayyad@gmail.com");
-
+List<String> ItemList = ['Fruits', "Banana", 'Grapes'];
+printItems(ItemList,'Fruits');
 }
 
  void greet({required String name ,String greeting  =  'Hello' }){
@@ -12,8 +13,15 @@ registerUser(username: "raghd", email: "raghdayyad@gmail.com");
 
  void registerUser({required String username ,required String email}){
   if(email.contains("@")){
-    print("your email is Valid /n User $username registered with email $email");
+    print("your email is Valid \n User $username registered with email $email");
   
   }else print('Invalid email for user $username');
  }
- 
+ void printItems(List<String> items ,[String title ='Item List']){
+   print(title);
+   int index=1;
+   for(String item in items){
+    print('$index . $item \n');
+    index++;
+   }
+ }
